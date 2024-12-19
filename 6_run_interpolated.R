f09f488cf4c91c82cc19bb357d83be2b027492cd
Andrea Sánchez Tapia <4183062+AndreaSanchezTapia@users.noreply.github.com.>
2024-12-19 14:29:04 -0500

    interpolate tracks and AIS, looping per day

--- 6_run_interpolated.R
+++ 6_run_interpolated.R
@@ -12,7 +12,9 @@ tracks
 # david was running interpolated for each date
 # query for one day
 #notice the {.x} to loop later
-interpolated <- readr::read_file("queries/05_david_interpolated_positions_Nov.sql")
+interpolated_albatross <- readr::read_file("queries/05_david_interpolated_positions_Nov.sql")
+interpolated_AIS <- readr::read_file("queries/06_david_AIS_interpolated_Nov.sql")
+interpolated <- interpolated_albatross
 
 # a function to loop through dates 
 create_table <-  function(.x) {
@@ -43,3 +45,27 @@ DATES <- lubridate::date(tracks$datetime) %>% unique() %>% sort()
 furrr::future_map(DATES, ~create_table(.x))
 plan(sequential)
 
+### AIS
+interpolated <- interpolated_AIS
+# a function to loop through dates 
+create_table <-  function(.x) {
+  # create the query
+  query <- glue::glue(interpolated)
+  # name the table to create a partitioned table by date
+  table_name <- paste0(project_id, ".", dataset, ".orben_AIS_interpolate$",format.Date(.x, "%Y%m%d"))
+  # do the query
+  bq_project_query(project_id,
+                   query = query,
+                   destination_table = table_name,
+                   configuration = list(
+                     query = list(
+                       time_partitioning = 
+                         list(#field = "time",#error with field but partition ok
+                           type = "DAY"))
+                   )
+  )
+}
+parallel::detectCores()
+plan(multisession, workers = parallel::detectCores() - 2)
+furrr::future_map(DATES, ~create_table(.x))
+plan(sequential)
--- queries/06_david_AIS_interpolated_Nov.sql
+++ queries/06_david_AIS_interpolated_Nov.sql
@@ -1,5 +1,3 @@
-# Copied from the original query by David and adapted to run for the new data
-
 CREATE TEMP FUNCTION today() AS (timestamp('{.x}'));
 
 CREATE TEMP FUNCTION weight_average_lons(lon float64, lon2 float64, timeto float64, timeto2 float64) AS 
@@ -20,47 +18,56 @@ CREATE TEMP FUNCTION radians(x float64) AS (
 );
 
 WITH 
-times_table AS (
+times_table as (
 SELECT * FROM UNNEST(GENERATE_TIMESTAMP_ARRAY(today(), 
-                                TIMESTAMP_ADD(today(), interval 24 * 60 - 1 minute),
+                                TIMESTAMP_ADD(today(), interval 24*60 - 1 minute),
                                 INTERVAL 5 minute)) AS time
 ),
 
-
 position_table AS 
 (SELECT 
-  study,
-  species,
-  BirdID,
+  seg_id,
+  ssvid,
   lat,
   lon,
-  TIMESTAMP(datetime) AS timestamp
+  timestamp
  FROM
-`scratch_andrea_ttl100.orben_tracks_Nov_no_wkt`
-WHERE DATE(datetime) = DATE(today())
+`pipe_ais_v3_published.messages`
+WHERE DATE(timestamp) = DATE(today())
+AND clean_segs
+AND lat > 15
+AND lat < 65
+AND (
+lon > 135 OR lon < -125
+)
+-- (141.133754449339
+-- 18.393021483871
+-- 179.999962735264
+-- 59.5676433514839)
 ),
 
 leaded_positions AS (
 SELECT
-  study,
-  species,
-  BirdID,
+  seg_id,
+  ssvid,
   lat,
-  LEAD(lat,1) OVER (PARTITION BY species,  BirdID ORDER BY timestamp) AS lat2,
+  LEAD(lat,1) OVER (PARTITION BY seg_id,
+  ssvid ORDER BY timestamp) AS lat2,
   lon,
-  LEAD(lon,1) OVER (PARTITION BY species, BirdID ORDER BY timestamp) AS lon2,
+  LEAD(lon,1) OVER (PARTITION BY seg_id,
+  ssvid ORDER BY timestamp) AS lon2,
   timestamp,
-  LEAD(timestamp,1) OVER (PARTITION BY species, BirdID ORDER BY timestamp) AS timestamp2,
+  LEAD(timestamp,1) OVER (PARTITION BY seg_id,
+  ssvid ORDER BY timestamp) AS timestamp2,
 FROM
 position_table),
 
 
 candidate_positions AS (
-      SELECT
-        study,
-        species,
-        BirdID,
-        time,
+      SELECT        
+          seg_id,
+          ssvid,
+          time,
         TIMESTAMP_DIFF(timestamp2,
           time,
           SECOND) + 1e-8 AS timeto2,
@@ -79,13 +86,13 @@ candidate_positions AS (
        timestamp <= time
        AND timestamp2 > time  
         ),
+
+
         interpolated_positions AS (
       SELECT
         (lat*timeto2 + lat2*timeto)/(timeto+timeto2) AS lat_center,
         reasonable_lon(weight_average_lons(lon, lon2, timeto, timeto2)) AS lon_center,
         *
       FROM candidate_positions)
-SELECT 
-* 
-FROM
-interpolated_positions
+
+SELECT * FROM interpolated_positions
